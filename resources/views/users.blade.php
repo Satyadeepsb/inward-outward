548b1152bd76002cf6769b328d8d31267de911c9
sagar-kanse <sagarkanse191@gmail.com>
2019-03-14 16:00:41 +0530

    extra button removed.

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -12,12 +12,6 @@
                    style="margin-top: 5px">
                     <b>Create</b>&nbsp;<i class="fa fa-plus-circle" aria-hidden="true"></i></a>
             </div>
-            <div class="col-md-1">
-                <a href="/user/0"
-                   class="btn btn-default btn-sm pull-right"
-                   style="margin-top: 5px">
-                    <b>Create</b>&nbsp;<i class="fa fa-plus-circle" aria-hidden="true"></i></a>
-            </div>
         </div>
         <table class="table table-striped table-hover " style="border: 1px solid lightgray;">
             <thead>
