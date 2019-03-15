4294636704f881c6f2dbe262935d132849eec2e8
Satyadeepsb <satyadeepsb@gmail.com>
2019-03-15 19:10:14 +0530

    seeder added

---
+++ database/seeds/DocumentTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DocumentTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\Document::create(['name' => 'Aadhar Card']);
+        App\Document::create(['name' => 'Pan Card']);
+        App\Document::create(['name' => 'Reshan Card']);
+        App\Document::create(['name' => '7/12 Utara']);
+    }
+}
