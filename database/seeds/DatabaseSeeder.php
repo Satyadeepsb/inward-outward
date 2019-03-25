a1f369e3182953b433c984eb05fff100060a93d7
Satyadeepsb <satyadeepsb@gmail.com>
2019-03-25 19:34:19 +0530

    new seeds added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,12 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class); // Prod done
-         $this->call(DocumentTableSeeder::class); // Prod done
-        //$this->call(SettingsTableSeeder::class); // Prod done
-        //$this->call(DepartmentsTableSeeder::class); // Prod done
-        //$this->call(DistrictsTableSeeder::class); // Prod done
-        //$this->call(TalukasTableSeeder::class); // Prod done
-        //$this->call(ActionTableSeeder::class);
+        $this->call(UsersTableSeeder::class); // Prod done
+        $this->call(DocumentTableSeeder::class); // Prod done
+        $this->call(SettingsTableSeeder::class); // Prod done
+        $this->call(DepartmentsTableSeeder::class); // Prod done
+        $this->call(DistrictsTableSeeder::class); // Prod done
+        $this->call(TalukasTableSeeder::class); // Prod done
+        $this->call(ActionTableSeeder::class);
     }
 }
